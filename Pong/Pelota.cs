a2ff531b42c9612da1efb6e5c071d22015955416
Gonzako <al341522@uji.es>
2016-05-02 19:37:13 +0200

    Form1 and Pelota improved.

--- Pong/Pelota.cs
+++ Pong/Pelota.cs
@@ -4,16 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing
 
 namespace Pong
 {
     class Pelota
     {
         private PictureBox bola;
+        private int velocidadTotal;
+        private int velocidadX;
+        private int velocidadY;
+        private int velocidadBase = 25;
+        private byte sentidoX, sentidoY;
+        private Random rand;
+        private Point pos;
 
         public Pelota(PictureBox bola)
         {
-
+            rand = new Random();
+            this.bola = bola;
+            velocidadTotal = velocidadBase;
+            velocidadX = rand.Next(velocidadTotal*20/100, velocidadTotal*70/100);
+            velocidadY = (int)Math.Sqrt(velocidadTotal*velocidadTotal-velocidadX*velocidadX);
+            while (sentidoX == 0) { sentidoX = (byte)rand.Next(-1, 2); }
+            while (sentidoY == 0) { sentidoY = (byte)rand.Next(-1, 2); }
+            velocidadX *= sentidoX;
+            velocidadY *= sentidoY;
         }
        
 
