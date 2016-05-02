3a115882e93a48f08dfb8e2d6078e73acce9d658
Gonzako <al341522@uji.es>
2016-05-03 00:25:40 +0200

    Added Raqueta

--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Pong
@@ -16,6 +16,8 @@ namespace Pong
         private Raqueta jugIzquierda;
         private Raqueta jugDerecha;
         private Pelota bola;
+        private System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        
 
         public Pong()
         {
@@ -25,13 +27,19 @@ namespace Pong
             jugIzquierda = new Raqueta(JugadorIzq, Keys.W, Keys.S);
             jugDerecha = new Raqueta(JugadorDer, Keys.Up, Keys.Down);
             bola = new Pelota(Pelota);
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+            myTimer.Interval = 33;
+            myTimer.Start();
+
 
         }
+
 
         private void Pong_KeyDown(object sender, KeyEventArgs e)
         {
             jugIzquierda.KeyDown(e.KeyCode);
             jugDerecha.KeyDown(e.KeyCode);
+            
          
         }
 
@@ -42,9 +50,25 @@ namespace Pong
 
         }
 
+        private void Pong_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
+        {
+            jugIzquierda.Actualizar();
+            jugDerecha.Actualizar();
+            bola.Mover();
+            bola.Choque(jugDerecha.Pala.Location.Y);
+            bola.Choque(jugIzquierda.Pala.Location.Y);
+        }
+
+
         private void Pong_Paint(object sender, PaintEventArgs e)
         {
 
+            
         }
     }
 }
--- Pong/Pelota.cs
+++ Pong/Pelota.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing
+using System.Drawing;
 
 namespace Pong
 {
@@ -14,24 +14,54 @@ namespace Pong
         private int velocidadTotal;
         private int velocidadX;
         private int velocidadY;
-        private int velocidadBase = 25;
-        private byte sentidoX, sentidoY;
+        private int velocidadBase = 5;
+        private int sentidoX, sentidoY;
         private Random rand;
         private Point pos;
-
+        
+        public Point Pos { get { return pos; } }
+ 
         public Pelota(PictureBox bola)
         {
             rand = new Random();
             this.bola = bola;
             velocidadTotal = velocidadBase;
-            velocidadX = rand.Next(velocidadTotal*20/100, velocidadTotal*70/100);
+            velocidadX = rand.Next(velocidadTotal*3/10, velocidadTotal*7/10);
             velocidadY = (int)Math.Sqrt(velocidadTotal*velocidadTotal-velocidadX*velocidadX);
             while (sentidoX == 0) { sentidoX = (byte)rand.Next(-1, 2); }
             while (sentidoY == 0) { sentidoY = (byte)rand.Next(-1, 2); }
             velocidadX *= sentidoX;
             velocidadY *= sentidoY;
+            pos = bola.Location;
         }
-       
+
+        public void Choque(int Y)
+        {
+            //mira si choca. Si cierto, cambia la velocidad
+            if (((pos.X <= 24 && pos.X >= 12) || (pos.X >= 760 && pos.X <= 772)) && (pos.Y >= Y || pos.Y <= Y + 50))
+            {
+                sentidoX *= -1;
+                velocidadTotal += 1;
+                velocidadX = rand.Next(velocidadTotal * 3 / 10, velocidadTotal * 7 / 10);
+                velocidadY = (int)Math.Sqrt(velocidadTotal * velocidadTotal - velocidadX * velocidadX);
+                velocidadX *= sentidoX;
+                velocidadY *= sentidoY;
+            }
+        }
+        
+        public void Mover()
+        {
+            if (pos.Y + 10 >= 580 || pos.Y <= 0)
+            {
+                sentidoY = -sentidoY;
+                velocidadY *= sentidoY;
+            }
+
+            pos.X += velocidadX;
+            pos.Y += velocidadY;
+            bola.Location = pos;
+        }
+
 
     }
 }
